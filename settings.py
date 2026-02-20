783ca516893d3d48885fb2851f1c70a0987753df
krimerty71 <a13ksandr0v.ant0n3@gmail.com>
2026-02-20 21:34:06 +1000

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,28 @@
+# Настройки игры
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+FPS = 60
+TITLE = "MARS: 1984 - Колонизация"
+
+# Цвета (RGB)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 50, 50)
+GREEN = (50, 255, 50)
+BLUE = (50, 50, 255)
+YELLOW = (255, 255, 50)
+ORANGE = (255, 150, 50)
+PURPLE = (150, 50, 255)
+GRAY = (100, 100, 100)
+DARK_RED = (150, 0, 0)
+MARS_RED = (193, 68, 14)
+MARS_SKY = (255, 200, 150)
+
+# Размер клетки
+CELL_SIZE = 32
+MAP_WIDTH = 40
+MAP_HEIGHT = 30
+
+# Пути к ресурсам
+SPRITE_PATH = "assets/sprites/"
+FONT_PATH = "assets/font/pixel.ttf"
